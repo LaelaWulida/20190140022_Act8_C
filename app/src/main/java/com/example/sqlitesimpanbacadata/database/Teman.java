ecf67686da085a59090aa3b08e84c84cbf92bb00
LaelaWulida <laela27811@gmail.com>
2021-06-12 07:44:37 +0700

    Initial commit

---
+++ app/src/main/java/com/example/sqlitesimpanbacadata/database/Teman.java
@@ -0,0 +1,42 @@
+package com.example.sqlitesimpanbacadata.database;
+
+import java.io.Serializable;
+
+public class Teman {
+    String Id;
+    String nama;
+    String telepon;
+
+    public Teman() {
+    }
+
+    public Teman(String id, String nama, String telepon) {
+        this.Id = id;
+        this.nama = nama;
+        this.telepon = telepon;
+    }
+
+    public String getId() {
+        return Id;
+    }
+
+    public void setId(String id) {
+        this.Id = id;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public String getTelepon() {
+        return telepon;
+    }
+
+    public void setTelepon(String telepon) {
+        this.telepon = telepon;
+    }
+}
